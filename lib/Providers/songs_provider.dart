3e5807510245d0231d0d038ee54e55b13e1f4827
Satyam Shrestha <52965655+mrxstm@users.noreply.github.com>
2025-09-07 22:43:12 +0545

    song provider is created which carries business logic and notify changes in state

--- lib/Providers/songs_provider.dart
+++ lib/Providers/songs_provider.dart
@@ -1,25 +1,85 @@
 
 import 'package:flutter/material.dart';
 import 'package:offline_music_player/Model/song_model.dart';
+import 'package:offline_music_player/Service/mp3files_scan.dart';
 
 class SongProvider extends ChangeNotifier {
-  // list of all the songs
-  List<Song> songs = [];
 
-  // song currently being played
-  Song? currentSong;
 
-  //playing or pause
-  bool? isPlaying;
+  // songs list
+      List<Song> songs = [];   //list of all songs
+      List<Song> favouriteSongs = []; //list of favourite songs
 
-  //repeat
-  bool? isRepeat;
+  // fetching song
 
-  //shuffle
-  bool? isShuffle;
+      SongScanner songScanner = SongScanner();
 
-  //functions
+      Future<void> fetchSongs() async {
+        try {
+          final response = await songScanner.scanAllSongs();
+          songs = response;
+          notifyListeners();
+        } catch(e) {
+          print("Failed to fetch songs: $e");
+        }
+      }
 
-  void is
+
+    // song currently being played
+    Song? currentSong;
+
+    //playing or pause
+    bool isPlaying = false;
+
+    //repeat
+    bool isRepeat = false;
+
+    //shuffle
+    bool isShuffle = false;
+
+  //functions :
+      // to play
+      void play() {
+        isPlaying = true;
+        notifyListeners();
+      }
+
+      // to pause
+      void pause() {
+        isPlaying = false;
+        notifyListeners();
+      }
+
+      // on loop
+      void toggleRepeat() {
+        isRepeat = !isRepeat;
+        notifyListeners();
+      }
+
+      // on loop
+      void shuffle() {
+        isShuffle = !isShuffle;
+        notifyListeners();
+      }
+
+      // to mark the songs favourite
+      void toggleFavourite(Song song) {
+        song.isFavourite = !song.isFavourite;
+
+        if(song.isFavourite) {
+          favouriteSongs.add(song);
+        } else {
+          favouriteSongs.remove(song);
+        }
+        notifyListeners();
+      }
+
+      //setting current song
+      void setCurrentSong(Song song) {
+        currentSong = song;
+        isPlaying = true;
+        notifyListeners();
+      }
+
 
 }
